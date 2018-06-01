10ca48a8750e39638a502f5c16eba0c0d2f0de0b
wangboning <meiniepan@163.com>
2018-06-01 18:46:40 +0800

    积分页面

---
+++ app/src/main/java/com/wuyou/user/mvp/score/ScoreGainedRecordActivity.java
@@ -0,0 +1,22 @@
+package com.wuyou.user.mvp.score;
+
+import android.os.Bundle;
+
+import com.wuyou.user.R;
+import com.wuyou.user.view.activity.BaseActivity;
+
+/**
+ * Created by Solang on 2018/6/1.
+ */
+
+public class ScoreGainedRecordActivity extends BaseActivity {
+    @Override
+    protected int getContentLayout() {
+        return R.layout.activity_score_gained_record;
+    }
+
+    @Override
+    protected void bindView(Bundle savedInstanceState) {
+
+    }
+}
