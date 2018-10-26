bef225b0011b25f5b9d85fa693977f2b7334b9d1
wangboning <meiniepan@163.com>
2018-10-26 11:43:02 +0800

    create 志愿者项目

---
+++ app/src/main/java/com/wuyou/user/mvp/volunteer/VolunteerProjListActivity.java
@@ -0,0 +1,21 @@
+package com.wuyou.user.mvp.volunteer;
+
+import android.os.Bundle;
+
+import com.wuyou.user.view.activity.BaseActivity;
+
+/**
+ * Created by Solang on 2018/10/26.
+ */
+
+public class VolunteerProjListActivity extends BaseActivity {
+    @Override
+    protected int getContentLayout() {
+        return 0;
+    }
+
+    @Override
+    protected void bindView(Bundle savedInstanceState) {
+
+    }
+}
