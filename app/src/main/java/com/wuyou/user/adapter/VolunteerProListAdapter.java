70ba1f219979f8e669376435da20275e919bf727
wangboning <meiniepan@163.com>
2018-10-30 16:12:00 +0800

    create 志愿项目列表UI

--- app/src/main/java/com/wuyou/user/adapter/VolunteerProListAdapter.java
+++ app/src/main/java/com/wuyou/user/adapter/VolunteerProListAdapter.java
@@ -9,6 +9,7 @@ import com.gs.buluo.common.widget.recyclerHelper.BaseQuickAdapter;
 import com.wuyou.user.Constant;
 import com.wuyou.user.R;
 import com.wuyou.user.data.api.VolunteerProjectBean;
+import com.wuyou.user.util.CommonUtil;
 import com.wuyou.user.util.EosUtil;
 import com.wuyou.user.util.glide.GlideUtils;
 
@@ -27,24 +28,44 @@ public class VolunteerProListAdapter extends BaseQuickAdapter<VolunteerProjectBe
     @Override
     protected void convert(BaseHolder holder, VolunteerProjectBean item) {
         int need = 0, participate = 0;
+        float rewardsMin = 0, rewardsMax = 0;
+        boolean isFirst = true;
         for (VolunteerProjectBean.PositionsBean e : item.positions
                 ) {
             need += e.amount;
             participate += e.enrolled.size();
+            float rew = Float.parseFloat(e.rewards.replaceAll("EOS", "").replaceAll(" ",""));
+            if (isFirst) {
+                isFirst = false;
+                rewardsMin = rewardsMax = rew;
+            } else {
+                if (rew < rewardsMin) {
+                    rewardsMin = rew;
+                } else if (rew > rewardsMax) {
+                    rewardsMax = rew;
+                }
+            }
+        }
+        String rewards;
+        if (rewardsMin == rewardsMax){
+            rewards = CommonUtil.formatPrice(rewardsMin);
+        }else {
+            rewards = CommonUtil.formatPrice(rewardsMin)+" - "+CommonUtil.formatPrice(rewardsMax);
         }
         holder.setText(R.id.tv_volunteer_pro_name, item.name)
-                .setText(R.id.tv_volunteer_pro_need, need+"")
-                .setText(R.id.tv_volunteer_pro_participate, participate+"");
+                .setText(R.id.tv_volunteer_pro_need, need + "")
+                .setText(R.id.tv_volunteer_pro_rewards, rewards)
+                .setText(R.id.tv_volunteer_pro_participate, participate + "");
         TextView isEnd = holder.getView(R.id.tv_volunteer_pro_is_end);
-        if (EosUtil.isOverdue(item.enroll_end_time)){
+        if (EosUtil.isOverdue(item.enroll_end_time)) {
             isEnd.setBackgroundResource(R.drawable.bac_gray_left_4_round);
             isEnd.setText("已结束");
-        }else {
+        } else {
             isEnd.setBackgroundResource(R.drawable.bac_blue_left_4_round);
             isEnd.setText("招募中");
         }
         ImageView logo = holder.getView(R.id.iv_volunteer_pro_logo);
-        GlideUtils.loadRoundCornerImage(mContext, Constant.IPFS_URL+item.logofile,logo);
+        GlideUtils.loadRoundCornerImage(mContext, Constant.IPFS_URL + item.logofile, logo);
     }
 
 }
--- app/src/main/java/com/wuyou/user/mvp/volunteer/VolunteerProListActivity.java
+++ app/src/main/java/com/wuyou/user/mvp/volunteer/VolunteerProListActivity.java
@@ -3,6 +3,8 @@ package com.wuyou.user.mvp.volunteer;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -16,9 +18,11 @@ import com.wuyou.user.adapter.VolunteerProListAdapter;
 import com.wuyou.user.data.EoscDataManager;
 import com.wuyou.user.data.api.ListRowResponse;
 import com.wuyou.user.data.api.VolunteerProjectBean;
+import com.wuyou.user.mvp.block.BlockDetailActivity;
 import com.wuyou.user.util.RxUtil;
 import com.wuyou.user.view.activity.BaseActivity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
@@ -30,8 +34,11 @@ import butterknife.BindView;
 public class VolunteerProListActivity extends BaseActivity {
     @BindView(R.id.rv_volunteer)
     RefreshRecyclerView recyclerView;
+    @BindView(R.id.et_volunteer_pro_list)
+    EditText etSearch;
     VolunteerProListAdapter adapter;
     List<VolunteerProjectBean> data;
+
 
     @Override
     protected int getContentLayout() {
@@ -42,6 +49,42 @@ public class VolunteerProListActivity extends BaseActivity {
     protected void bindView(Bundle savedInstanceState) {
         setTitleText(getString(R.string.volunteer_project));
         initRv();
+        etSearch.setOnEditorActionListener((v, actionId, event) -> {
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                String searchText = etSearch.getText().toString().trim();
+                doSearch(searchText);
+                return true;
+            }
+            return false;
+        });
+    }
+
+    private void doSearch(String searchText) {
+        if (searchText.length() == 0) {
+            if (data.size() > 0) {
+                recyclerView.showContentView();
+                adapter.setNewData(data);
+            }
+            return;
+        }
+        List<VolunteerProjectBean> searchData = new ArrayList<>();
+        for (VolunteerProjectBean e : data
+                ) {
+            if (e.name.contains(searchText)) {
+                searchData.add(e);
+            }
+        }
+        if (searchData.size() == 0) {
+            recyclerView.showEmptyView("暂无符合志愿项目");
+        } else {
+            recyclerView.showContentView();
+            adapter.setNewData(searchData);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
         getVolunteerProList();
     }
 
